38fcd94e6f4777a0c8d3f9f2bbe4d3c788ba39ce
Gulzat <gulzatyessen@gmail.com>
2026-06-15 15:14:16 +0500

    add frontend deploy

---
+++ frontend/server.cjs
@@ -0,0 +1,12 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+app.use(express.static(path.join(__dirname, 'dist')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Frontend running on port ${PORT}`));
