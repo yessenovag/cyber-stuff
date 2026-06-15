7502ca3d7b7db88750d43c171755569bc2177f80
Gulzat <gulzatyessen@gmail.com>
2026-06-15 15:28:47 +0500

    fix css import casing

--- frontend/src/pages/Adminfeedback.tsx
+++ frontend/src/pages/Adminfeedback.tsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { useAuth } from "../context/AuthContext";
-import "./AdminFeedback.css";
+import "./Adminfeedback.css";
 
 type FeedbackItem = {
   id: number;
